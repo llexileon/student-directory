dcb130d152229a3e2361de7f681f26542213bb2b
Robert Leon <llexi@hotmail.com>
2014-04-01 17:18:42 +0100

    Added directory ruby file with list of students

---
+++ directory.rb
@@ -0,0 +1,26 @@
+
+# Student directory work in progress April 1st 2014
+
+puts "The students of my cohort at Makers Academy"
+puts "-------------------------------------------"
+
+puts "Steve Musgrave"
+puts "Sroop Sunar"
+puts "Colin Marshall"
+puts "Josh Fail-Brown"
+puts "Louise Lai"
+puts "Robin Doble"
+puts "Alex Wong"
+puts "Scott Dimmock"
+puts "Muhanad Al-Rubaiee"
+puts "Shelley Hope"
+puts "Will Hall"
+puts "Oliver Delevingne"
+puts "Nico"
+puts "Apostolis"
+puts "Stefania"
+puts "Jennifer Jentle"
+puts "Robert Leon"
+puts "Emma Williams"
+puts "Joey Wolf"
+puts "Julie Walker"
