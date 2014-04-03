762738cbe7e21f0a10591fa11e20761f69c4a29a
Robert Leon <llexi@hotmail.com>
2014-04-03 18:59:53 +0100

    Centering text for end user

--- directory.rb
+++ directory.rb
@@ -2,21 +2,21 @@
 # Student directory work in progress April 1st 2014 #
 
 def print_header 
-	print "The students of my cohort at Makers Academy\n"
-    print "-------------------------------------------\n"
+	print "The students at Makers Academy!\n".center(50)
+    print "------------------------------\n".center(5)
 end
 
 # Ask for user input to compile student data #
 
 def input_students
-	print "Please enter the name of the first student you'd like to add to the database \n"
+	print "Please enter the name of the first student you'd like to add to the database \n".center(50)
 	#get the first name
 	name = gets.chomp
-	print "Please enter the cohort of the first student you'd like to add to the database \n"
+	print "Please enter the cohort of the first student you'd like to add to the database \n".center(50)
 	cohort = gets.chomp
-	print "Please enter the home city of the first student you'd like to add to the database \n"
+	print "Please enter the home city of the first student you'd like to add to the database \n".center(50)
 	city = gets.chomp
-	print "Please enter the hobby of the first student you'd like to add to the database \n"
+	print "Please enter the hobby of the first student you'd like to add to the database \n".center(50)
 	hobby = gets.chomp
 
 
@@ -27,8 +27,8 @@ def input_students
 		# add the student hash to the array
 		students << {:name => name, :cohort => cohort, :city => city, :hobby => hobby}
 		#get another name from the user
-	print "Please enter the next student name, followed by thier cohort, city, and hobby \n"
-	print "( Or to finish, just hit your enter or return key 4 times ;D ) \n"
+	print "Please enter the next student name, followed by thier cohort, city, and hobby \n".center(50)
+	print "( Or to finish, just hit your enter or return key 4 times ;D ) \n".center(50)
 		name = gets.chomp
 		cohort = gets.chomp
 		city = gets.chomp
@@ -89,11 +89,11 @@ end
 # Print total student count with .length #
 def print_footer(students)
 	if students.length <= 1
-		print ""
-		print "Overall, we have #{students.length} great student \n"
-	else 
-		print ""
-		print "Overall, we have #{students.length} great students \n"
+		print "\n"
+		print "Overall, we have #{students.length} great student \n".center(50)
+	else
+		print "\n"
+		print "Overall, we have #{students.length} great students \n".center(50)
 	end
 end
 
